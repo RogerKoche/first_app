7a282ffac262ee8843a31abfd9daaaecda9c3ab1
U-Token\Shadowrak <Shadowrak@Token.(none)>
2012-07-13 14:01:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e4a872e63c861e2cc0c80c6301db5984824eaa6f6165d1daeeb2e75a9f3f5aca7b33040ee932483c29cf889d79c419fad58ffce6d16721a5bee0793cd3518306'
